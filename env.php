6250fe2f09d023d320727a2880eff9ae5a28b919
gamal <gamal.pratama@scm.co.id>
2016-05-25 11:54:58 +0700

    init project

---
+++ env.php
@@ -0,0 +1,14 @@
+<?php
+
+$env_config = [
+	// 'home'		=>	$_SERVER['HTTP_HOST'],
+	// 'host'		=>	$_SERVER['HTTP_HOST'],
+	'db_name'	=> 	'news.sctv',
+	'db_host'	=>	'localhost',
+	'db_user'	=>	'root',
+	'db_pass'	=>	'root'
+
+];
+
+
+?>
---
+++ wp-content/themes/news-sctv/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package news_sctv_theme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'news-sctv' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'news-sctv' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'news-sctv' ), 'news-sctv', '<a href="http://underscores.me/" rel="designer">gamal</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>

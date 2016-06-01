7c8b43772a7e36de8f4f8bd6eac5bcf3e182557f
gamal <gamal.pratama@scm.co.id>
2016-06-01 11:14:05 +0700

    add footer

--- sneaky/wp-content/themes/news-sctv/footer.php
+++ sneaky/wp-content/themes/news-sctv/footer.php
@@ -14,8 +14,27 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-
+		<div class="site-info clearfix">
+			<div class="site-info-copyright col-xs-6">
+				<span>
+					<i class="fa fa-copyright" aria-hidden="true"></i>
+					COPYRIGHT 2015 SCTV. ALL RIGHTS RESERVED.
+				</span>
+			</div>
+			<div class="site-info-live hidden-xs col-sm-2">
+				live streaming
+			</div>
+			<div class="site-info-social hidden-xs col-sm-1">
+				<div>
+					
+				</div>
+			</div>
+			<div class="site-info-extra hidden-xs col-sm-3">
+				<div class="text-right">
+					Tautan Luar <i class="fa fa-chevron-down"></i>	
+				</div>
+				
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
